ea75c12550a03ab8d66da9c65b2fb11901ebd356
danielbrock4 <daniel.brock4@outlook.com>
2021-07-22 16:42:58 -0500

    filtered retirement list by departments

--- queries.sql
+++ queries.sql
@@ -114,3 +114,88 @@ SELECT * FROM emp_count_by_dept
 SELECT * FROM salaries
 ORDER BY to_date DESC
 ;
+SELECT * FROM salaries
+ORDER BY to_date DESC
+;
+SELECT emp_no,
+	first_name,
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s ON (e.emp_no = s.emp_no) 
+	INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01')
+;	
+SELECT * FROM emp_info
+;
+-- List of Managers per Department
+SELECT 
+	dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.first_name,
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info	
+FROM dept_manager as dm
+	INNER JOIN departments AS d 
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce 
+		ON (dm.emp_no = ce.emp_no)
+;
+SELECT 
+	ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+;
+SELECT 
+	ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+-- INTO sales_requested_list
+FROM retirement_info AS ri
+	INNER JOIN dept_emp AS de
+		ON (ri.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)		
+;
+SELECT * FROM dept_info
+;
+SELECT
+	emp_no,
+	first_name,
+	last_name,
+	dept_name
+INTO sales_retirement_list	
+FROM dept_info
+WHERE dept_name = 'Sales'
+;
+SELECT * FROM dept_info;
+SELECT *
+INTO sales_dev_retirement_list
+FROM dept_info
+WHERE dept_name IN ('Sales', 'Development')
